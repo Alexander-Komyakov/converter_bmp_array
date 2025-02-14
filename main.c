8b07a02736cc0a6bc75295d2e4bf720e9c4439cd
Alexander Komyakov <agkomiakov@hex.team>
2025-02-14 16:29:10 +0300

    version 0.1

--- main.c
+++ main.c
@@ -2,26 +2,71 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int check_bmp(char * path) {
+int main(int argc, char * argv[]) {
+    char * path = argv[1];
     printf("%s\n", path);
+
     FILE * image = fopen(path, "rb");
     if (image == NULL) {
         perror("File not opened");
         return 1;
     }
-    char *begin_file = (char*) malloc(sizeof(char)*2);
-    fread(begin_file, sizeof(char)*2, 2, image);
-    if (begin_file[0] == 66 && begin_file[1] == 77) {
+    char *header_file = (char*) malloc(sizeof(char)*1);
+    fread(header_file, sizeof(char)*2, 1, image);
+    if (header_file[0] != 66 || header_file[1] != 77) {
         fclose(image);
-        free(begin_file);
-        return 0;
+        free(header_file);
+        printf("Erorr, it's not bmp\n");
+        return 1;
     }
-    fclose(image);
-    free(begin_file);
-    return 1;
-}
 
-int main() {
-    printf("BMP = %i\n", check_bmp("aboba.bmp"));
-    return 0;
+    uint32_t *size_file = (uint32_t*) malloc(sizeof(uint32_t));
+    fread(size_file, sizeof(uint32_t), 1, image);
+
+    printf("Size file: %d\n", size_file[0]);
+
+    uint32_t *zero_file = (uint32_t*) malloc(sizeof(uint32_t));
+    fread(zero_file, sizeof(uint32_t), 1, image);
+
+    uint32_t *position_file = (uint32_t*) malloc(sizeof(uint32_t));
+    fread(position_file, sizeof(uint32_t), 1, image);
+
+
+    int body_position_file = position_file[0] - 14;
+    uint32_t *skip_info_file = (uint32_t*) malloc(sizeof(uint32_t)*body_position_file);
+    fread(skip_info_file, sizeof(uint32_t) * body_position_file, 1, image);
+
+    printf("body position file: %d\n", body_position_file);
+
+    int body_size_file = (size_file[0] - (position_file[0]))/2;
+    //uint16_t *body_file = (uint16_t*) malloc(sizeof(uint16_t)*body_size_file);
+    uint16_t *one_file = (uint16_t*) malloc(sizeof(uint16_t));
+
+    char * save_path = argv[2];
+    printf("%s\n", save_path);
+
+    FILE * save_image = fopen(save_path, "wt");
+
+    fprintf(save_image, "#include <stdio.h>\n");
+    fprintf(save_image, "\nuint16_t my_image[%d] = {\n", *size_file);
+    for (int i = 0; i < (size_file[0]/2) - position_file[0]; i++) {
+        if (fread(one_file, sizeof(uint16_t), 1, image) == 4) {
+            break;
+        }
+        if ( i % 10 == 0) {
+            fprintf(save_image, "\n");
+        }
+        uint16_t swapest_bits = (one_file[0] << 8) | (one_file[0] >> 8);
+        fprintf(save_image, "0x%X, ", swapest_bits);
+    }
+    fprintf(save_image, "};\n");
+    fclose(image);
+    free(header_file);
+    free(zero_file);
+    free(position_file);
+    free(skip_info_file);
+    free(one_file);
+
+    fclose(save_image);
+    return 1;
 }
